8ff95c89e2b6aa971990cb5696133b974e342afd
Smicer12 <developer.smicer@gmail.com>
2021-08-26 12:41:55 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@ int main(void)
 {	/** printf displays both variables
 	* and strings to the screen
 	*/
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
