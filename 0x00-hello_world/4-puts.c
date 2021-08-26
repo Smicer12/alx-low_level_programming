b64d3d94dd6c90cd2ea4f7052d415deeb990e793
Smicer12 <developer.smicer@gmail.com>
2021-08-26 12:44:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@ int main(void) /* begins proramm */
 {	/**
 	* puts function prints something to the screen.
 	**/
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle\n");
 		return (0);
 }
