604383bba7d4c7d6bcd1fae9fb6c67a42b6d2529
Smicer12 <developer.smicer@gmail.com>
2021-08-26 13:32:10 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 int main(void)
 	/* c data types */
 {
-	printf("size of a char:", sizeof(char));
-	printf("size of an int:", sizeof(int));
-	printf("size of a long int:", sizeof(long int));
-	printf("size of a long long int:", sizeof(long long int));
-	printf("size of a float:", sizeof(float));
+	printf("size of a char:", sizeof(char); );
+	printf("size of an int:", sizeof(int); );
+	printf("size of a long int:", sizeof(long int); );
+	printf("size of a long long int:", sizeof(long long int); );
+	printf("size of a float:", sizeof(float); );
 
 		return (0);
 }
