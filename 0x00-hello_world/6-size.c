0ae1a86a5887c9894382f6ce0ae4feaefd11e3d2
Smicer12 <developer.smicer@gmail.com>
2021-08-26 13:14:49 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+	/* c data types */
+{	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("%c size of a char:", a);
+	printf("%d size of an int:", b);
+	printf("%ld size of a long int:",c);
+	printf("%lld size of a long long int:",d);
+	printf("%f size of a float:",e);
+
+		return (0);
+}
