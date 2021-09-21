e019e9ab0cbec692b16ed06bd38ab120e796ea05
Smicer12 <developer.smicer@gmail.com>
2021-09-21 21:07:04 +0300

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,10 @@
-
-#include <stdio.h>
-
+#include <stdio.h> 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		putchar('\n');
-		return 0;
+		return ;
 	}
 	putchar(*s);
 	_puts_recursion(s + 1);
