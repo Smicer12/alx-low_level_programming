e5070d9df28fff8cac1d10bee3b56f2bd397d268
smicer12 <developer.smicer12@gmail.com>
2021-11-01 14:37:56 +0300

    task 2 of func & nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
-#include <stdio.h>                                                                                                                            
-#include <unistd.h>
+
+int _putchar(char c);
+void print_alphabet_x10(void);
