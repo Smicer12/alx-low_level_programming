14d4955616821750a9eb5e10daf842b1a310bc59
smicer12 <developer.smicer12@gmail.com>
2021-11-12 09:55:08 +0300

    more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,2 @@
-#include <stdio.h>
-#include <ctype.h>
-
-int _isupper(int c)
-{
-	if( isupper(c))
-		{printf("%c is uppercase:", c);}
-
-	else
-		{ printf("%c is lowercase:", c); }
-
-
-}
+int _putchar(char c);
+int _isupper(int c);
